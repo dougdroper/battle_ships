a990cee360651dddc4b85bd11dc7800d8b8d3a28
Douglas Roper <dougdroper@yahoo.com>
2013-03-03 20:35:43 +0000

    available positions

--- lib/fleet.rb
+++ lib/fleet.rb
@@ -2,10 +2,17 @@ class Fleet
   attr_reader :fleet
   def initialize
     @fleet = [Ship.new(5), Ship.new(4), Ship.new(3), Ship.new(3), Ship.new(2)]
+    @available_positions = [(0...10), (0...10)]
   end
 
   def position
-    @fleet
+    @fleet.map {|s| s.position(@available_positions) }
+  end
+
+  def place_ship(ship)
+    x, y, length, direction = ship
+    dx, dy = direction == :across ? [1, 0] : [0, 1]
+    (0 ... length).map{ |i| [x + i * dx, y + i * dy] }
   end
 end
 
@@ -14,5 +21,28 @@ class Ship
   attr_accessor :x, :y, :direction
   def initialize(length)
     @length = length
+  end
+
+  def direction
+    :across
+  end
+
+  def position(available_positions)
+    x,y = random_position(available_positions)
+  end
+
+  private
+
+  def random_position(available_positions)
+    if direction == :across
+      x_pos = available_positions.first.to_a
+      range = x_pos.slice(start(x_pos), length)
+      available_positions = available_positions - range
+      [range.first, 0]
+    end
+  end
+
+  def start(available_positions)
+    (available_positions.length - length)
   end
 end
--- spec/lib/fleet_spec.rb
+++ spec/lib/fleet_spec.rb
@@ -9,3 +9,13 @@ describe Fleet do
     Fleet.new.position.should == [5,4,3,3,2]
   end
 end
+
+describe Ship do
+  it "places the ship on a available positions" do
+    Ship.new(5).position([(0...5), (0...1)]).should == [0,0]
+  end
+
+  it "places the ship on a available positions" do
+    Ship.new(5).position([(1...6), (0...1)]).should == [1,0]
+  end
+end
